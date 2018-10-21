445c51f9bcdaa53fc234b797f89c12f45d5fa8d6
matatsuna <matatsuna@gmail.com>
2018-10-21 23:57:41 +0900

    fix value

---
+++ Value/index.js
@@ -0,0 +1,30 @@
+const valueKind = 11;
+const value = [
+    {
+        "label": "width",
+        "kind": valueKind,
+        "documentation": "画面の横幅",
+        "insertText": "width "
+    },
+    {
+        "label": "height",
+        "kind": valueKind,
+        "documentation": "画面の縦幅",
+        "insertText": "height "
+    },
+    {
+        "label": "mouseX",
+        "kind": valueKind,
+        "documentation": "マウスのX座標",
+        "insertText": "mouseX "
+    },
+    {
+        "label": "mouseY",
+        "kind": valueKind,
+        "documentation": "マウスのY座標",
+        "insertText": "mouseY "
+    }
+];
+
+
+export default value;
