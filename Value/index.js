46503036a76e6844a6b0d6ef15b4b9d8b4159c58
matatsuna <matatsuna@gmail.com>
2018-10-22 17:31:50 +0900

    fix value

--- Value/index.js
+++ Value/index.js
@@ -23,6 +23,102 @@ const value = [
         "kind": valueKind,
         "documentation": "マウスのY座標",
         "insertText": "mouseY "
+    },
+    {
+        "label": "HALF_PI",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "HALF_PI "
+    },
+    {
+        "label": "PI",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "PI "
+    },
+    {
+        "label": "QUARTER_PI",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "QUARTER_PI "
+    },
+    {
+        "label": "TAU",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "TAU "
+    },
+    {
+        "label": "TWO_PI",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "TWO_PI "
+    },
+    {
+        "label": "focused",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "focused "
+    },
+    {
+        "label": "frameCount",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "frameCount "
+    },
+    {
+        "label": "frameRate",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "frameRate "
+    },
+    {
+        "label": "pixelHeight",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "pixelHeight "
+    },
+    {
+        "label": "pixelWidth",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "pixelWidth "
+    },
+    {
+        "label": "key",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "key "
+    },
+    {
+        "label": "keyCode",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "keyCode "
+    },
+    {
+        "label": "pmouseX",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "pmouseX "
+    },
+    {
+        "label": "pmouseY",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "pmouseY "
+    },
+    {
+        "label": "mousePressed",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "mousePressed "
+    },
+    {
+        "label": "mouseButton",
+        "kind": valueKind,
+        "documentation": "",
+        "insertText": "mouseButton "
     }
 ];
 
