389e44ac76a5ab4aead18e54d328154bc476fb0f
matatsuna <matatsuna@gmail.com>
2018-10-22 11:54:31 +0900

    add finction

--- Function/index.js
+++ Function/index.js
@@ -1,16 +1,66 @@
-const fs = require('fs');
-export default (() => {
-    try {
-        fs.readdir('.', function (err, files) {
-            if (err) throw err;
-            var fileList = files.filter(function (file) {
-                return fs.statSync(file).isFile() && /.*\.js$/.test(file); //絞り込み
-            })
-            console.log(fileList);
-        });
-    }
-    catch (err) {
-        console.warn(err);
+import fs from 'fs';
+import path from 'path';
+
+class Function {
+    constructor() {
+        this.FunctionKind = 2;
+        this.Function = [
+            {
+                "label": "setup",
+                "kind": this.FunctionKind,
+                "documentation": "文字列",
+                "insertText": "for"
+            }
+        ];
     }
 
-});
+    async setup() {
+        let pdeFiles = await this.getPdeFiles();
+        let pdeData = [];
+        pdeFiles.forEach((element) => {
+            pdeData[element.filename] = element.data;
+        });
+        this.Function = this.Function.map((block) => {
+            const file = block.insertText;
+            const text = pdeData[file];
+            block.insertText = text;
+            return block;
+        });
+        return this.Function;
+    }
+
+    getPdeFiles() {
+        return new Promise((resolve) => {
+            try {
+                fs.readdir("Function/pde", (async (err, files) => {
+                    if (err) throw err;
+                    files = files.filter((file) => {
+                        return /.pde$/.test(file)
+                    })
+                    let pdeFiles = await Promise.all(
+                        files.map(async (file) => {
+                            let filepath = path.join(__dirname, "pde", file);
+                            return {
+                                filename: file.replace('.pde', ''),
+                                data: await this.getPdeFile(filepath)
+                            }
+                        })
+                    );
+                    resolve(pdeFiles);
+                }));
+            }
+            catch (err) {
+                console.warn(err);
+            }
+        });
+    }
+
+    getPdeFile(file) {
+        return new Promise((resolve) => {
+            fs.readFile(file, 'utf-8', ((err, data) => {
+                resolve(data);
+            }))
+        });
+    }
+}
+export default Function;
--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 import fs from 'fs';
+
 import Class from './Class';
 import value from './Value';
-console.log(value);
-console.log(Class);
-let json = Class.concat(value);
+import Function from './Function';
 
-fs.writeFile('./processing-complete.json', JSON.stringify(json));
+async function create() {
+    // console.log(value);
+    // console.log(Class);__dirname
+
+    let func = new Function();
+    let f = await func.setup();
+    let json = Class.concat(value).concat(f);
+    console.log(json);
+    fs.writeFile('./processing-complete.json', JSON.stringify(json));
+}
+
+create();
+
